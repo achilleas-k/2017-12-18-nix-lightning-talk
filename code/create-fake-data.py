588afb37e1a9d114279c7b3a09a84d10bf186f34
Achilleas Koutsou <achilleas.k@gmail.com>
2017-12-18 03:09:39 +0100

    Transposed data

--- code/create-fake-data.py
+++ code/create-fake-data.py
@@ -5,6 +5,6 @@ wave = np.sin(np.arange(0, 100, 0.1))
 data = np.array([
     wave + np.random.random(len(wave)),
     wave + np.random.random(len(wave)),
-])
+]).transpose()
 
 np.savez("bunch-o-numbers.npz", data=data)
